ce23a14122769378f3c5920d8914f7ff223c2991
Utsav Patel <utsav.kalash131297@gmail.com>
2022-03-18 18:05:55 -0400

    Corrected path in constants.py.

--- Classical Multi-View Representation Learning/constants.py
+++ Classical Multi-View Representation Learning/constants.py
@@ -8,9 +8,9 @@ if mp.cpu_count() >= 10:
     VALIDATION_IMAGE_PATH = "/freespace/local/as3503/536/image_features/image_val_embeddings.pkl"
     TEST_IMAGE_PATH = "/freespace/local/as3503/536/image_features/image_test_embeddings.pkl"
 
-    TRAIN_TEXT_PATH = "/freespace/local/as3503/536/embeddings_all_mean/text_" + TEXT_ELEMENT + "_train_embeddings.pkl"
-    VALIDATION_TEXT_PATH = "/freespace/local/as3503/536/embeddings_all_mean/text_" + TEXT_ELEMENT + "_val_embeddings.pkl"
-    TEST_TEXT_PATH = "/freespace/local/as3503/536/embeddings_all_mean/text_" + TEXT_ELEMENT + "_test_embeddings.pkl"
+    TRAIN_TEXT_PATH = "/freespace/local/as3503/536/embeddings_all_means/text_" + TEXT_ELEMENT + "_train_embeddings.pkl"
+    VALIDATION_TEXT_PATH = "/freespace/local/as3503/536/embeddings_all_means/text_" + TEXT_ELEMENT + "_val_embeddings.pkl"
+    TEST_TEXT_PATH = "/freespace/local/as3503/536/embeddings_all_means/text_" + TEXT_ELEMENT + "_test_embeddings.pkl"
 
 else:
     TRAIN_IMAGE_PATH = "D:/My_Files/Rutgers/Courses/536 - Machine Learning/Project/" \
