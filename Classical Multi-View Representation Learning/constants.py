52ea93e9a48afe052f6a748c95027465e5e90241
Utsav Patel <utsav.kalash131297@gmail.com>
2022-03-18 15:23:15 -0400

    Updated path in constants.py file. Removed multiprocessing from train_cca.py.

--- Classical Multi-View Representation Learning/constants.py
+++ Classical Multi-View Representation Learning/constants.py
@@ -2,32 +2,31 @@ import multiprocessing as mp
 
 SEARCH_POOL = 1000
 TEXT_ELEMENT = "title"
-MIN_LATENT_DIMENSION = 1
-MAX_LATENT_DIMENSION = 20
+TYPE_EMBEDDING = "image"
 
 if mp.cpu_count() >= 10:
-    TRAIN_IMAGE_PATH = "/freespace/local/as3503/536/image_features/image_train_embeddings.pkl"
-    VALIDATION_IMAGE_PATH = "/freespace/local/as3503/536/image_features/image_val_embeddings.pkl"
-    TEST_IMAGE_PATH = "/freespace/local/as3503/536/image_features/image_test_embeddings.pkl"
+    TRAIN_IMAGE_PATH = "/freespace/local/as3503/536/entire/image_features/image_train_embeddings.pkl"
+    VALIDATION_IMAGE_PATH = "/freespace/local/as3503/536/entire/image_features/image_val_embeddings.pkl"
+    TEST_IMAGE_PATH = "/freespace/local/as3503/536/entire/image_features/image_test_embeddings.pkl"
 
-    TRAIN_TEXT_PATH = "/freespace/local/as3503/536/embeddings_mean/text_" + TEXT_ELEMENT + "_train_embeddings.pkl"
-    VALIDATION_TEXT_PATH = "/freespace/local/as3503/536/embeddings_mean/text_" + TEXT_ELEMENT + "_val_embeddings.pkl"
-    TEST_TEXT_PATH = "/freespace/local/as3503/536/embeddings_mean/text_" + TEXT_ELEMENT + "_test_embeddings.pkl"
+    TRAIN_TEXT_PATH = "/freespace/local/as3503/536/entire/embeddings_mean/text_" + TEXT_ELEMENT + "_train_embeddings.pkl"
+    VALIDATION_TEXT_PATH = "/freespace/local/as3503/536/entire/embeddings_mean/text_" + TEXT_ELEMENT + "_val_embeddings.pkl"
+    TEST_TEXT_PATH = "/freespace/local/as3503/536/entire/embeddings_mean/text_" + TEXT_ELEMENT + "_test_embeddings.pkl"
 
 else:
     TRAIN_IMAGE_PATH = "D:/My_Files/Rutgers/Courses/536 - Machine Learning/Project/" \
-                       "Cross-Modal-Representation-Learning/data/image_features/image_train_embeddings.pkl"
+                       "Cross-Modal-Representation-Learning/data/entire/image_features/image_train_embeddings.pkl"
     VALIDATION_IMAGE_PATH = "D:/My_Files/Rutgers/Courses/536 - Machine Learning/Project/"\
-                            "Cross-Modal-Representation-Learning/data/image_features/image_val_embeddings.pkl"
+                            "Cross-Modal-Representation-Learning/data/entire/image_features/image_val_embeddings.pkl"
     TEST_IMAGE_PATH = "D:/My_Files/Rutgers/Courses/536 - Machine Learning/Project/"\
-                      "Cross-Modal-Representation-Learning/data/image_features/image_test_embeddings.pkl"
+                      "Cross-Modal-Representation-Learning/data/entire/image_features/image_test_embeddings.pkl"
 
     TRAIN_TEXT_PATH = "D:/My_Files/Rutgers/Courses/536 - Machine Learning/Project/"\
-                      "Cross-Modal-Representation-Learning/data/embeddings_mean/text_" + TEXT_ELEMENT +\
+                      "Cross-Modal-Representation-Learning/data/entire/embeddings_mean/text_" + TEXT_ELEMENT +\
                       "_train_embeddings.pkl"
     VALIDATION_TEXT_PATH = "D:/My_Files/Rutgers/Courses/536 - Machine Learning/Project/" \
-                           "Cross-Modal-Representation-Learning/data/embeddings_mean/text_" + TEXT_ELEMENT +\
+                           "Cross-Modal-Representation-Learning/data/entire/embeddings_mean/text_" + TEXT_ELEMENT +\
                            "_val_embeddings.pkl"
     TEST_TEXT_PATH = "D:/My_Files/Rutgers/Courses/536 - Machine Learning/Project/"\
-                     "Cross-Modal-Representation-Learning/data/embeddings_mean/text_" + TEXT_ELEMENT +\
+                     "Cross-Modal-Representation-Learning/data/entire/embeddings_mean/text_" + TEXT_ELEMENT +\
                      "_test_embeddings.pkl"
--- Classical Multi-View Representation Learning/train_cca.py
+++ Classical Multi-View Representation Learning/train_cca.py
@@ -1,12 +1,11 @@
 import pickle
-import multiprocessing as mp
 import numpy as np
 
 from datetime import datetime
 
 from cca_zoo.models import CCA
-from constants import TRAIN_IMAGE_PATH, TRAIN_TEXT_PATH, VALIDATION_IMAGE_PATH, VALIDATION_TEXT_PATH,\
-    MAX_LATENT_DIMENSION, MIN_LATENT_DIMENSION, SEARCH_POOL, TEXT_ELEMENT
+from constants import TRAIN_IMAGE_PATH, TRAIN_TEXT_PATH, VALIDATION_IMAGE_PATH, VALIDATION_TEXT_PATH, SEARCH_POOL,\
+    TEXT_ELEMENT, TYPE_EMBEDDING
 from helper import rank
 
 
@@ -34,24 +33,25 @@ def create_dataset(filepath: str):
     return data
 
 
-def compute_rank(data: list):
-
-    image_train_data = data[0]
-    image_val_data = data[1]
-    text_train_data = data[2]
-    text_val_data = data[3]
-    latent_dims = data[4]
+def compute_rank(latent_dims: int):
 
     cca = CCA(latent_dims=latent_dims)
     print('Starting Training for', latent_dims)
     print_current_time()
-    cca.fit((image_train_data, text_train_data))
+    if TYPE_EMBEDDING == "image":
+        cca.fit((image_train_data, text_train_data))
+    else:
+        cca.fit((text_train_data, image_train_data))
     print('Ending Training for', latent_dims)
     print_current_time()
 
     print('Start transforming and calculating ranks')
-    image_data_c, text_data_c = cca.transform((image_val_data, text_val_data))
-    medr, recall_k = rank(SEARCH_POOL, "image", image_data_c, text_data_c)
+    if TYPE_EMBEDDING == "image":
+        image_data_c, text_data_c = cca.transform((image_val_data, text_val_data))
+    else:
+        text_data_c, image_data_c = cca.transform((text_val_data, image_val_data))
+
+    medr, recall_k = rank(SEARCH_POOL, TYPE_EMBEDDING, image_data_c, text_data_c)
 
     print('latent dimensions', latent_dims, 'Median ', medr)
     print('latent dimensions', latent_dims, 'Recall', recall_k)
@@ -80,28 +80,29 @@ if __name__ == "__main__":
     print('Validation image shape', image_val_data.shape)
     print('Validation text shape', text_val_data.shape)
 
-    n_cores = int(mp.cpu_count())
-    print('Number of cores', n_cores)
-    p = mp.Pool(processes=1)
+    # n_cores = int(mp.cpu_count())
+    # print('Number of cores', n_cores)
+    # p = mp.Pool(processes=1)
 
-    final_list = [[image_train_data, image_val_data, text_train_data, text_val_data, x]
-                  for x in range(MIN_LATENT_DIMENSION, MAX_LATENT_DIMENSION+1)]
+    latent_dims_list = [1, 2, 5, 10, 15, 20, 30, 50, 75, 100, 150, 200, 300, 500, 700]
+    # final_list = [[image_train_data, image_val_data, text_train_data, text_val_data, x]
+    #               for x in range(MIN_LATENT_DIMENSION, MAX_LATENT_DIMENSION+1)]
     # final_list = range(1, MAX_LATENT_DIMENSION + 1)
-    results = p.map(compute_rank, final_list)
+    # results = p.map(compute_rank, final_list)
 
     medr_list = list()
     recall_k_list = list()
 
-    for result in results:
+    for latent_dims in latent_dims_list:
+        result = compute_rank(latent_dims)
         medr_list.append(result[0])
         recall_k_list.append(result[1])
 
     print(medr_list)
     print(recall_k_list)
 
-    with open('./results/cca_' + TEXT_ELEMENT + '_' + str(SEARCH_POOL) + "_" + str(MIN_LATENT_DIMENSION) + "_" +
-              str(MAX_LATENT_DIMENSION) + '.pkl', 'wb') as f:
-        pickle.dump({'medr': medr_list, 'recall_k': recall_k_list, 'latent_dims': range(1, MAX_LATENT_DIMENSION + 1)},
+    with open('./results/cca_' + TEXT_ELEMENT + '_' + str(SEARCH_POOL) + "_" + '.pkl', 'wb') as f:
+        pickle.dump({'medr': medr_list, 'recall_k': recall_k_list, 'latent_dims': latent_dims_list},
                     f)
 
 
