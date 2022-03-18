3776b4c8a7127b0c9bdfadead50a205597453ecc
Utsav Patel <utsav.kalash131297@gmail.com>
2022-03-18 17:39:04 -0400

    Changed path in constants.py file.

--- Classical Multi-View Representation Learning/constants.py
+++ Classical Multi-View Representation Learning/constants.py
@@ -4,28 +4,28 @@ TEXT_ELEMENT = "all"
 TYPE_EMBEDDING = "image"
 
 if mp.cpu_count() >= 10:
-    TRAIN_IMAGE_PATH = "/freespace/local/as3503/536/embeddings/image_train_embeddings.pkl"
-    VALIDATION_IMAGE_PATH = "/freespace/local/as3503/536/embeddings/image_val_embeddings.pkl"
-    TEST_IMAGE_PATH = "/freespace/local/as3503/536/embeddings/image_test_embeddings.pkl"
+    TRAIN_IMAGE_PATH = "/freespace/local/as3503/536/image_features/image_train_embeddings.pkl"
+    VALIDATION_IMAGE_PATH = "/freespace/local/as3503/536/image_features/image_val_embeddings.pkl"
+    TEST_IMAGE_PATH = "/freespace/local/as3503/536/image_features/image_test_embeddings.pkl"
 
-    TRAIN_TEXT_PATH = "/freespace/local/as3503/536/entire/embeddings_all_mean/text_" + TEXT_ELEMENT + "_train_embeddings.pkl"
-    VALIDATION_TEXT_PATH = "/freespace/local/as3503/536/entire/embeddings_all_mean/text_" + TEXT_ELEMENT + "_val_embeddings.pkl"
-    TEST_TEXT_PATH = "/freespace/local/as3503/536/entire/embeddings_all_mean/text_" + TEXT_ELEMENT + "_test_embeddings.pkl"
+    TRAIN_TEXT_PATH = "/freespace/local/as3503/536/embeddings_all_mean/text_" + TEXT_ELEMENT + "_train_embeddings.pkl"
+    VALIDATION_TEXT_PATH = "/freespace/local/as3503/536/embeddings_all_mean/text_" + TEXT_ELEMENT + "_val_embeddings.pkl"
+    TEST_TEXT_PATH = "/freespace/local/as3503/536/embeddings_all_mean/text_" + TEXT_ELEMENT + "_test_embeddings.pkl"
 
 else:
     TRAIN_IMAGE_PATH = "D:/My_Files/Rutgers/Courses/536 - Machine Learning/Project/" \
-                       "Cross-Modal-Representation-Learning/data/entire/image_features/image_train_embeddings.pkl"
+                       "Cross-Modal-Representation-Learning/data/image_features/image_train_embeddings.pkl"
     VALIDATION_IMAGE_PATH = "D:/My_Files/Rutgers/Courses/536 - Machine Learning/Project/"\
-                            "Cross-Modal-Representation-Learning/data/entire/image_features/image_val_embeddings.pkl"
+                            "Cross-Modal-Representation-Learning/data/image_features/image_val_embeddings.pkl"
     TEST_IMAGE_PATH = "D:/My_Files/Rutgers/Courses/536 - Machine Learning/Project/"\
-                      "Cross-Modal-Representation-Learning/data/entire/image_features/image_test_embeddings.pkl"
+                      "Cross-Modal-Representation-Learning/data/image_features/image_test_embeddings.pkl"
 
     TRAIN_TEXT_PATH = "D:/My_Files/Rutgers/Courses/536 - Machine Learning/Project/"\
-                      "Cross-Modal-Representation-Learning/data/entire/embeddings_mean/text_" + TEXT_ELEMENT +\
+                      "Cross-Modal-Representation-Learning/data/embeddings_mean/text_" + TEXT_ELEMENT +\
                       "_train_embeddings.pkl"
     VALIDATION_TEXT_PATH = "D:/My_Files/Rutgers/Courses/536 - Machine Learning/Project/" \
-                           "Cross-Modal-Representation-Learning/data/entire/embeddings_mean/text_" + TEXT_ELEMENT +\
+                           "Cross-Modal-Representation-Learning/data/embeddings_mean/text_" + TEXT_ELEMENT +\
                            "_val_embeddings.pkl"
     TEST_TEXT_PATH = "D:/My_Files/Rutgers/Courses/536 - Machine Learning/Project/"\
-                     "Cross-Modal-Representation-Learning/data/entire/embeddings_mean/text_" + TEXT_ELEMENT +\
+                     "Cross-Modal-Representation-Learning/data/embeddings_mean/text_" + TEXT_ELEMENT +\
                      "_test_embeddings.pkl"
