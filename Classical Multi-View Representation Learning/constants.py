d343f3b0a0622671761413b57f1e96a0e09a1399
Utsav Patel <utsav.kalash131297@gmail.com>
2022-03-20 17:47:09 -0400

    Updated constants.py and visualization.py.

--- Classical Multi-View Representation Learning/constants.py
+++ Classical Multi-View Representation Learning/constants.py
@@ -22,18 +22,18 @@ if mp.cpu_count() >= 10:
 
 else:
     TRAIN_IMAGE_PATH = "D:/My_Files/Rutgers/Courses/536 - Machine Learning/Project/" \
-                       "Cross-Modal-Representation-Learning/data/image_features/image_train_embeddings.pkl"
+                       "Cross-Modal-Representation-Learning/data/professor/embeddings_train1.pkl"
     VALIDATION_IMAGE_PATH = "D:/My_Files/Rutgers/Courses/536 - Machine Learning/Project/"\
-                            "Cross-Modal-Representation-Learning/data/image_features/image_val_embeddings.pkl"
+                            "Cross-Modal-Representation-Learning/data/professor/embeddings_val1.pkl"
     TEST_IMAGE_PATH = "D:/My_Files/Rutgers/Courses/536 - Machine Learning/Project/"\
-                      "Cross-Modal-Representation-Learning/data/image_features/image_test_embeddings.pkl"
+                      "Cross-Modal-Representation-Learning/data/professor/embeddings_test1.pkl"
 
     TRAIN_TEXT_PATH = "D:/My_Files/Rutgers/Courses/536 - Machine Learning/Project/"\
-                      "Cross-Modal-Representation-Learning/data/embeddings_mean/text_" + TEXT_ELEMENT +\
-                      "_train_embeddings.pkl"
+                      "Cross-Modal-Representation-Learning/data/professor/" + TEXT_ELEMENT +\
+                      "_embeddings_train.pkl"
     VALIDATION_TEXT_PATH = "D:/My_Files/Rutgers/Courses/536 - Machine Learning/Project/" \
-                           "Cross-Modal-Representation-Learning/data/embeddings_mean/text_" + TEXT_ELEMENT +\
-                           "_val_embeddings.pkl"
+                           "Cross-Modal-Representation-Learning/data/professor/" + TEXT_ELEMENT +\
+                           "_embeddings_val.pkl"
     TEST_TEXT_PATH = "D:/My_Files/Rutgers/Courses/536 - Machine Learning/Project/"\
-                     "Cross-Modal-Representation-Learning/data/embeddings_mean/text_" + TEXT_ELEMENT +\
-                     "_test_embeddings.pkl"
+                     "Cross-Modal-Representation-Learning/data/professor/" + TEXT_ELEMENT +\
+                     "_embeddings_test.pkl"
--- Classical Multi-View Representation Learning/visualization.py
+++ Classical Multi-View Representation Learning/visualization.py
@@ -2,7 +2,7 @@ import pickle
 import numpy as np
 
 from cca_zoo.models import CCA
-from constants import TRAIN_IMAGE_PATH, TRAIN_TEXT_PATH
+from constants import TRAIN_IMAGE_PATH, TRAIN_TEXT_PATH, VALIDATION_IMAGE_PATH, VALIDATION_TEXT_PATH
 
 
 def create_dataset(filepath: str):
@@ -32,6 +32,7 @@ for ind in range(len(ids)):
 
 text_train_data = create_dataset(TRAIN_TEXT_PATH)[0]
 
+print('Start Training')
 cca = CCA(latent_dims=500, random_state=0)
 cca.fit((text_train_data, image_train_data))
 print("Training is completed")
@@ -41,12 +42,12 @@ print("Transforming")
 text_train_r, img_train_r = cca.transform((text_train_data, image_train_data))
 # img_test_r, text_test_r = cca.transform((img_test, text_test))
 
-# "title": "Chicken Lasagna", "id": "547c1e56aa"      // train
+# "title": "Chicken Lasagna", "id": "2907ae25b5"      // train
 # "title": "Lasagna", "id": "4b12be6889"              // train
 # "title": "Salad", "id": "ad0c7fc652"                // train
 # "title": "Chicken Salad", "id": "4033b570e0"        // train
 
-required_ids = ["547c1e56aa", "4b12be6889", "ad0c7fc652", "4033b570e0"]
+required_ids = ["2907ae25b5", "4b12be6889", "ad0c7fc652", "4033b570e0"]
 
 required_test_embedding = text_train_r[id_to_ind[required_ids[0]]] - text_train_r[id_to_ind[required_ids[1]]] + \
                           text_train_r[id_to_ind[required_ids[2]]]
