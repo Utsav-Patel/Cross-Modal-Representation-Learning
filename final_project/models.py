90cd0711f2992c69e21feb5e64e9087df749e9d0
Aniket Vijaykumar Sanap <as3503@mac.cs.rutgers.edu>
2022-04-27 19:18:03 -0400

    Added transformer

--- final_project/models.py
+++ final_project/models.py
@@ -28,3 +28,55 @@ class ImageEncoder(nn.Module):
     def forward(self, img):
         outputs = self.main(img)
         return outputs.last_hidden_state
+
+
+class SinusoidalPositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        """
+        Args:
+            x: Tensor, shape [seq_len, batch_size, embedding_dim]
+        """
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
+
+
+class CrossModalAttention(nn.Module):
+    def __init__(self, model_dim=768, n_heads=2, n_layers=2, num_image_patches=197, num_classes=2, drop_rate=0.1):
+        super().__init__()
+        self.text_pos_embed = SinusoidalPositionalEncoding(model_dim, dropout=drop_rate)
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, model_dim))
+        self.sep_token = nn.Parameter(torch.zeros(1, 1, model_dim))
+        self.image_pos_embed = nn.Parameter(torch.zeros(1, num_image_patches + 1, model_dim))
+        self.image_pos_drop = nn.Dropout(p=drop_rate)
+        layers = nn.TransformerEncoderLayer(
+            d_model=model_dim,
+            nhead=n_heads,
+            batch_first=True
+        )
+        self.encoder = nn.TransformerEncoder(layers, num_layers=n_layers)
+        self.cls_projection = nn.Linear(model_dim, num_classes)
+        
+    def forward(self, image_features, text_features, src_key_padding_mask=None):
+        cls_token = self.cls_token.expand(image_features.shape[0], -1, -1)
+        image_features = torch.cat((cls_token, image_features), dim=1)
+        image_features = image_features + self.image_pos_embed
+        image_features = self.image_pos_drop(image_features)
+        
+        text_features = self.text_pos_embed(text_features)
+        
+        sep_token = self.sep_token.expand(image_features.shape[0], -1, -1)
+        transformer_input = torch.cat((image_features, sep_token, text_features), dim=1)
+        transformer_outputs = self.encoder(transformer_input, src_key_padding_mask=src_key_padding_mask)
+        cls_outputs = transformer_outputs[:, 0, :]
+        return self.cls_projection(cls_outputs)
