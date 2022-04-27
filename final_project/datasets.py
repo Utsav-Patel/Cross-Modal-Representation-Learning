e7500fc6e17f33b95dcffb3bdb43becedc8e707e
Aniket Vijaykumar Sanap <as3503@mac.cs.rutgers.edu>
2022-04-27 17:23:33 -0400

    Initial Project commit

---
+++ final_project/datasets.py
@@ -0,0 +1,114 @@
+import torch
+import os
+import numpy as np
+import json
+import lmdb
+import random
+import pickle
+
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from PIL import Image
+from io import BytesIO
+
+
+def get_train_transforms(mean, std):
+    return transforms.Compose([
+        transforms.Resize(256),
+        transforms.ColorJitter(hue=.05, saturation=.05),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(),
+        transforms.RandomRotation(20, resample=Image.BILINEAR),
+        transforms.RandomCrop((224,224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean, std=std)
+    ])
+
+def get_val_transforms(mean, std):
+    return transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop((224,224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean, std=std)
+    ])
+
+
+class Recipe1MDataset(Dataset):
+    """
+    Dataset class for Recipe1M
+    Attributes:
+    part: train/val/test
+    transform: image transforms
+
+    Returns:
+    text/image pair
+    """
+    def __init__(
+        self, 
+        lmdb_file=f'/common/home/as3503/as3503/courses/cs536/dataset/Recipe1M.lmdb',
+        part='', transform=None, resolution=256):
+
+        assert part in ['', 'train', 'val', 'test'], "part has to be in ['', 'train', 'val', 'test']"
+
+        if transform is None:
+            if part == 'val':
+                self.transform = get_train_transforms([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+            else:
+                self.transform = get_val_transforms([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+
+        self.resolution = resolution
+
+        dirname = os.path.dirname(lmdb_file)
+        path = os.path.join(dirname, 'keys.json')
+
+        with open(path, 'r') as f:
+            self.keys = json.load(f)
+
+        self.keys = [x for x in self.keys if x['with_image']]
+
+        if part:
+            self.keys = [x for x in self.keys if x['partition']==part]
+
+        self.env = lmdb.open(
+            lmdb_file,
+            max_readers=32,
+            readonly=True,
+            lock=False,
+            readahead=False,
+            meminit=False,
+        )
+
+        if not self.env:
+            raise IOError('Cannot open lmdb dataset', lmdb_file)
+
+    def __len__(self):
+        return len(self.keys)
+
+    def _load_recipe(self, rcp):
+        rcp_id = rcp['id']
+
+        with self.env.begin(write=False) as txn:
+            # print("Loading recipe")
+            key = f'title-{rcp_id}'.encode('utf-8')
+            title = txn.get(key).decode('utf-8')
+
+            key = f'ingredients-{rcp_id}'.encode('utf-8')
+            ingredients = txn.get(key).decode('utf-8')
+
+            key = f'instructions-{rcp_id}'.encode('utf-8')
+            instructions = txn.get(key).decode('utf-8')
+
+            key = f'{self.resolution}-{rcp_id}'.encode('utf-8')
+            img_bytes = txn.get(key)
+
+        txt = '\n'.join([title, ingredients, instructions])
+
+        buffer = BytesIO(img_bytes)
+        img = Image.open(buffer)
+        img = self.transform(img)
+        return txt, img
+
+    def __getitem__(self, index):
+        rcp_key = self.keys[index]
+        txt, img = self._load_recipe(rcp_key)
+        return txt, img
---
+++ final_project/models.py
@@ -0,0 +1,30 @@
+import torch
+from torch import nn
+import math
+from torchvision import models
+from transformers import BertConfig, BertModel, BertTokenizer, ViTModel
+import pdb
+
+class TextEncoder(nn.Module):
+    def __init__(self, num_heads, num_hidden_layers):
+        super().__init__()
+
+        config = BertConfig(num_attention_heads=num_heads, num_hidden_layers=num_hidden_layers)
+        self.main = BertModel(config)
+        bert_model_state_dict = BertModel.from_pretrained('bert-base-uncased').state_dict()
+        embedding_weights = {x:bert_model_state_dict[x] for x in bert_model_state_dict if 'embedding' in x}
+        self.main.load_state_dict(embedding_weights, strict=False)
+    
+    def forward(self, input_ids, attention_mask, token_type_ids):
+        bert_outputs = self.main(input_ids, attention_mask, token_type_ids, output_attentions=True)
+        return bert_outputs.pooler_output, bert_outputs.last_hidden_state[:, 1:, :]
+
+
+class ImageEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.main = ViTModel.from_pretrained('google/vit-base-patch16-224-in21k')
+
+    def forward(self, img):
+        outputs = self.main(img)
+        return outputs.pooler_output, outputs.last_hidden_state[:, 1:, :]
